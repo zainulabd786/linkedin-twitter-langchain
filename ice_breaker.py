390172255aadebc72bc0612d49855586ea7a2797
Zainul Abideen <waeezazeem@gmail.com>
2024-05-04 20:52:02 +0530

    feat: create and integrate output parser with LCEL

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,6 +7,7 @@ from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.twitter import scrape_user_tweets
+from output_parsers import summary_parser
 
 
 def ice_break_with(name: str) -> str:
@@ -24,11 +25,13 @@ def ice_break_with(name: str) -> str:
             1. A short summary
             2. two interesting facts about them
             
-            Use both information from twitter and LinkedIn
+            Use both information from twitter and LinkedIn\n
+            {format_instructions}
             """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information", "twitter_posts"], template=summary_template
+        input_variables=["information", "twitter_posts"], template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(
@@ -37,7 +40,8 @@ def ice_break_with(name: str) -> str:
         # openai_api_base=os.environ["OPENAI_API_BASE"],
     )
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    #chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
 
     print("res", res)

05c735a19567dee2f51789df37df422a534da6d3
Zainul Abideen <waeezazeem@gmail.com>
2024-05-04 16:14:48 +0530

    feat: find linkedin url and scrape data then generate llm resp

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,15 +4,17 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 import os
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == "__main__":
-    load_dotenv()
+def ice_break_with(name:str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
-        given the information {information} about a person I want you to create:
-        1. A short summary
-        2. two interesting facts about them
-        """
+            given the information {information} about a person I want you to create:
+            1. A short summary
+            2. two interesting facts about them
+            """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
@@ -21,11 +23,16 @@ if __name__ == "__main__":
     llm = ChatOpenAI(
         temperature=0,
         model_name=os.environ["OPENAI_API_MODEL"],
-        openai_api_base=os.environ["OPENAI_API_BASE"],
+        # openai_api_base=os.environ["OPENAI_API_BASE"],
     )
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="", mock=True)
+    # linkedin_data = scrape_linkedin_profile(linkedin_profile_url="", mock=True)
     res = chain.invoke(input={"information": linkedin_data})
 
     print("res", res)
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    ice_break_with("Zainul Abideen")
